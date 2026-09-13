f6a425698e012546ed1a2d274ff655be4a736af5
User-Teixeira <yuni3099@gmail.com>
2026-09-13 13:44:27 +0900

    style: use theme color for stop button

--- src/index.ts
+++ src/index.ts
@@ -115,7 +115,7 @@ function stopMessageTranslation(messageId: number): boolean {
   controller.abort();
   messageTranslationControllers.delete(messageId);
   setTranslationButtonState(messageId, false);
-  st_echo('info', 'Translation stopped');
+  st_echo('error', 'Translation stopped');
   return true;
 }
 
